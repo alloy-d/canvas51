64d527214fa7de6a5946a02b43f6cf96d45faf41
Adam Lloyd <adam@alloy-d.net>
2010-12-26 01:10:24 -0500

    Behold! The Big Bang!

--- awesome.js
+++ awesome.js
@@ -3,12 +3,16 @@ var Star = new Class({
                     this.options = options;
                     this.x = options.x;
                     this.y = options.y;
-                    this.brightness = options.brightness;
-                    this.color = options.color;
+                    this.brightness = options.brightness || 1;
+                    this.color = options.color || "white";
                 },
-    draw: function(context, width, height) {
+    draw: function(context, width, height, step) {
               var x = this.x * width / 1000;
               var y = this.y * height / 1000;
+              if (typeof(step) === 'number') {
+                  x = width / 2 + (step / 100) * (x - width / 2);
+                  y = height / 2 + (step / 100) * (y - height / 2);
+              }
 
               context.save();
               context.fillStyle = this.color;
@@ -23,30 +27,55 @@ var Star = new Class({
 var canvas = document.getElementById("awesome");
 var context = canvas.getContext("2d");
 
-function makeStarDrawer(numStars) {
+function makeStarDrawers(numStars) {
     var stars = [];
     var i = 0, c = 0;
     for (i = 0; i < numStars; i += 1) {
-        c = Math.floor(Math.random() * 150);
+        c = Math.floor(17 + Math.random() * 238);
         stars[stars.length] = new Star({
             x: Math.random() * 1000,
             y: Math.random() * 1000,
             brightness: Math.random() * Math.random() * 2,
-            color: "#" + (105+c).toString(16) + (180).toString(16) + (255-c).toString(16),
+            color: "#" + (000+c).toString(16) + (128).toString(16) + (255-c).toString(16),
         });
     }
 
-    return function() {
+    function drawStatic () {
         var width = window.innerWidth;
         var height = window.innerHeight;
         canvas.width = width;
         canvas.height = height;
 
-        Array.each(stars, function(star) { star.draw(context, width, height) });
+        Array.each(stars, function(star){ star.draw(context, width, height); });
     }
+
+    return {
+        draw: drawStatic,
+        bang: function() {
+            function drawStep(step) {
+                var width = window.innerWidth;
+                var height = window.innerHeight;
+                context.clearRect(0, 0, width, height);
+                if (step === 0) {
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+
+                Array.each(stars, function(star) {
+                    star.draw(context, width, height, step);
+                });
+
+                if (step < 100) {
+                    setTimeout(function(){drawStep(step+5);}, 5);
+                } else {
+                    window.addEvent('resize', drawStatic);
+                }
+            }
+            drawStep(0);
+        },
+    };
 }
 
-drawStars = makeStarDrawer(1000);
+drawers = makeStarDrawers(1000);
 
-window.addEvent('resize', drawStars);
-window.addEvent('domready', drawStars);
+window.addEvent('domready', drawers["bang"]);
