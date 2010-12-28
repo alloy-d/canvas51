9c66e5cc4e52068bca266c56ead8b0d86f7029c5
Adam Lloyd <adam@alloy-d.net>
2010-12-28 03:29:45 -0500

    Dash toward the big bang!

--- awesome.js
+++ awesome.js
@@ -61,6 +61,8 @@ var bangContext = bangCanvas.getContext("2d");
 var nameCanvas = document.getElementById("twinkling");
 var nameContext = nameCanvas.getContext("2d");
 
+var bangStarted = false;
+
 function makeStarDrawers(numStars) {
     var stars = [];
     var i = 0, c = 0, tmp, color;
@@ -156,13 +158,49 @@ function makeNameDrawers() {
     };
 }
 
+function drawDash() {
+    function drawDashStep(step) {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+
+        var fillColor = "#cc00";
+        var t = Math.floor(Math.sin(step) * 32.5);
+
+        fillColor += (180 + t).toString(16);
+
+        bangCanvas.width = width;
+        bangCanvas.height = height;
+
+        bangContext.save();
+        bangContext.fillStyle = fillColor;
+
+        bangContext.font = "48px Penshurst";
+        t = bangContext.measureText("DASH (X)!!!").width;
+
+        bangContext.fillText("DASH (X)!!!", (width - t) / 2, height * 0.7);
+        bangContext.restore();
+
+        if (!bangStarted) setTimeout(function(){drawDashStep(step+1);}, 50);
+    }
+    drawDashStep(0);
+}
+
+
 
 drawers = makeStarDrawers(1000);
 nameDrawers = makeNameDrawers();
 
-window.addEvent('domready', function () {
-    drawers["bang"](function () {
-        nameDrawers['draw']();
-        window.addEvent('resize', drawers['draw']);
-    });
+
+window.addEvent('load', drawDash);
+
+window.addEvent('keydown', function (event) {
+    if (bangStarted) return;
+    if (event.key === 'x') {
+        bangStarted = true;
+        drawers["bang"](function () {
+            nameDrawers['draw']();
+            window.addEvent('resize', drawers['draw']);
+        });
+    }
 });
+
