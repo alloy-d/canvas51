e7e7d466632317dd0508df96c3c3196a11adf262
Adam Lloyd <adam@alloy-d.net>
2010-12-30 01:12:47 -0500

    Look: a rudimentary platform in the sky!

--- awesome.js
+++ awesome.js
@@ -5,12 +5,15 @@ stars = makeStars("littlebang", 1000, twinkles.locations);
 horizon = makeHorizon("horizon");
 text = makeText("text");
 breakable = makeBreakable("littlebang", 500, 500, 100);
+ground = makeGround("ground");
 
 window.addEvent('load', horizon.draw);
+window.addEvent('load', ground.draw);
 window.addEvent('load', breakable.draw);
 window.addEvent('load', text.draw);
 
 window.addEvent('resize', horizon.draw);
+window.addEvent('resize', ground.draw);
 window.addEvent('resize', breakable.draw);
 
 window.addEvent('keydown', function (event) {
