4e8ba35757dbc0d801c9909150ca832e0897a7cf
Adam Lloyd <adam@alloy-d.net>
2011-01-06 02:02:09 -0500

    Alright, I give up. (Also, SO COOL.)

--- awesome.js
+++ awesome.js
@@ -23,8 +23,27 @@ var clearCanvas = function () {
     context.clearRect(0, 0, WIDTH, HEIGHT);
 }
 
-var font = makeFont(80, 150, 30);
-var locations = font.A(20, 20);
+var locations = (function (w, h, x, y, text) {
+    var font = makeFont(w, h);
+    var cx = x, cy = y;
+    var i = 0;
+    var locations = [];
+
+    for (i = 0; i < text.length; i += 1) {
+        if (text[i] === " ") {
+            cx = x;
+            cy += h + 50;
+            continue;
+        }
+        if (text[i] === ",") locations = locations.append(font.comma(cx, cy));
+        else locations = locations.append(font[text[i]](cx, cy));
+
+        cx += w + 6;
+    }
+
+    return locations;
+}(70, 130, 20, 20, "HAPPY BIRTHDAY, SOPHIE"));
+
 
 var horizon = makeHorizon();
 var star = makeStar(500, 500, 100);
@@ -54,7 +73,6 @@ var drawFrame = function () {
 
     FRAME += 1;
 }
-
 
 window.addEvent('load', function () {
     resizeCanvas();
--- starfont.js
+++ starfont.js
@@ -1,64 +1,46 @@
-function makeFont(width, height, density) {
-    var dx = width / density;
-    var dy = height / density;
+function makeFont(width, height) {
+    // Percentage to actual font width.
+    var w = function (d) { return width * d / 100; }
+    // Percentage to actual font height.
+    var h = function (d) { return height * d / 100; }
 
-    var genPoints = function (f, x, y) {
-        var cx = 0, cy = 0;
-        var tx = 0, ty = 0;
-        var points = [];
-
-        while (cx <= width && cy <= height) {
-            if (f.x) {
-                for (i = 0; i < f.x.length; i += 1) {
-                    ty = f.x[i](cx);
-                    if (ty !== null) points[points.length] = [x + cx, y + ty];
-                }
-            }
-            if (f.y) {
-                for (i = 0; i < f.y.length; i += 1) {
-                    tx = f.y[i](cy);
-                    if (tx !== null) points[points.length] = [x + tx, y + cy];
-                }
-            }
-            cx += dx;
-            cy += dy;
-        }
-
-        return points;
-    };
+    // Some random noise.
+    var r = function (n) { return 1-Math.floor(Math.random()*3); }
+    // Add randomness to a pair.
+    var rp = function (p) { return [p[0]+r(), p[1]+r()]; }
 
     return {
-        A: function (x, y) {
-            var f = {
-                x: [
-                    function (x) {
-                        if (x < width/2) {
-                            return height - height * (x)/(width/2);
-                        } else {
-                            return height * (x - width/2)/(width/2);
-                        }
-                    },
-                    function (x) {
-                        if (x > width/4 && x < 3*width/4 && (x % 2))
-                            return 2*height/3;
-                    }
-                ],
-            }
-
-            return genPoints(f, x, y);
+        A: function (x, y) { return []; },
+        B: function (x, y) { return []; },
+        D: function (x, y) { return []; },
+        E: function (x, y) { return []; },
+        H: function (x, y) {
+            return [
+                [x, y],
+                [x, y + h(20)],
+                [x, y + h(40)],
+                [x, y + h(60)],
+                [x, y + h(80)],
+                [x, y + h(100)],
+                [x + w(25), y + h(48)],
+                [x + w(50), y + h(48)],
+                [x + w(75), y + h(48)],
+                [x + w(100), y],
+                [x + w(100), y + h(20)],
+                [x + w(100), y + h(40)],
+                [x + w(100), y + h(60)],
+                [x + w(100), y + h(80)],
+                [x + w(100), y + h(100)],
+            ].map(rp);
         },
-        B: undefined,
-        D: undefined,
-        E: undefined,
-        H: undefined,
-        I: undefined,
-        O: undefined,
-        P: undefined,
-        R: undefined,
-        S: undefined,
-        T: undefined,
-        Y: undefined,
-        comma: undefined,
+        I: function (x, y) { return []; },
+        O: function (x, y) { return []; },
+        P: function (x, y) { return []; },
+        R: function (x, y) { return []; },
+        S: function (x, y) { return []; },
+        T: function (x, y) { return []; },
+        Y: function (x, y) { return []; },
+        comma: function (x, y) { return []; },
         space: function (x, y) { return []; },
     };
 }
