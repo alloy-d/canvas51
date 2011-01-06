60a6fba319301962c64a92c70d1b4a548c2b27e9
Adam Lloyd <adam@alloy-d.net>
2011-01-06 04:54:08 -0500

    Put a bunch of stars together, and you can see that they twinkle at the same rate.

--- twinkles.js
+++ twinkles.js
@@ -5,12 +5,13 @@ var TwinklingStar = new Class({
         if (this.brightness < 4) this.brightness = 4;
         this.color = "#eeeeee";
         this.stepSeed = Math.random() * Math.PI;
+        this.stepScale = Math.random() / 3;
     },
     draw: function() {
         var x = cx(this.pos.x);
         var y = cy(this.pos.y);
         var brightness = this.brightness;
-        brightness += Math.sin(FRAME * 0.3 + this.stepSeed);
+        brightness += Math.sin(FRAME * this.stepScale + this.stepSeed);
 
         context.save();
         context.fillStyle = this.color;
